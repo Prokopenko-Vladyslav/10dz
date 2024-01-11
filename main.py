d6cacd4de0f2657242292b493148b7f1857f3e1a
Vladyslav <proshavl@ukr.net>
2024-01-12 00:33:05 +0200

    1commit

---
+++ main.py
@@ -0,0 +1,72 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    # реалізація класу Name, що успадковується від Field
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate_phone()
+
+    def validate_phone(self):
+        if not self.value.isdigit() or len(self.value) != 10:
+            raise ValueError("Phone number must contain 10 digits.")
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        try:
+            self.phones.append(Phone(phone))
+        except ValueError as e:
+            print(e)
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        phone_found = False
+        for phone in self.phones:
+            if phone.value == old_phone:
+                phone.value = new_phone
+                phone.validate_phone()
+                phone_found = True
+                break
+        if not phone_found:
+            raise ValueError("Phone number not found for editing.")
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {
+            '; '.join(str(p) for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
